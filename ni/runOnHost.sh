81b1e37625dfb9280764129d4f2495a32be1a34b
Alex <djsasha777@mail.ru>
2024-11-01 21:40:50 +0300

    up

--- ni/runOnHost.sh
+++ ni/runOnHost.sh
@@ -2,17 +2,14 @@
 
 cp /tmp/* /host
 cp /wait.sh /host
-cp /init.sh /host
-cp /preinit.sh /host
-cp /install.sh /host
 
-/usr/bin/nsenter -m/proc/1/ns/mnt -- chmod u+x /tmp/install/wait.sh
-/usr/bin/nsenter -m/proc/1/ns/mnt -- chmod u+x /tmp/install/install.sh
-/usr/bin/nsenter -m/proc/1/ns/mnt -- chmod u+x /tmp/install/init.sh
-/usr/bin/nsenter -m/proc/1/ns/mnt -- chmod u+x /tmp/install/preinit.sh
+/usr/bin/nsenter -m/proc/1/ns/mnt -- chmod u+x /tmp/install/*
 
+/usr/bin/nsenter -m/proc/1/ns/mnt /tmp/install/softinstall.sh
 /usr/bin/nsenter -m/proc/1/ns/mnt /tmp/install/wait.sh
+
+/usr/bin/nsenter -m/proc/1/ns/mnt /tmp/install/preinit.sh
+
 /usr/bin/nsenter -m/proc/1/ns/mnt /tmp/install/init.sh
 
-/usr/bin/nsenter -m/proc/1/ns/mnt /tmp/install/wait.sh
 /usr/bin/nsenter -m/proc/1/ns/mnt /tmp/install/install.sh
