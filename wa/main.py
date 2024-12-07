854f27c474a33b6d6cea631f36f0cccfadd368f5
Alex <djsasha777@mail.ru>
2024-12-07 08:05:25 +0300

    update

--- wa/main.py
+++ wa/main.py
@@ -1,5 +1,9 @@
 import time
 from kubernetes import client, config, watch
+import os
+
+lbtype = os.getenv('LBTYPE')
+domain = os.getenv('DOMAIN')
 
 # Загружаем конфигурацию кластера Kubernetes
 config.load_incluster_config()
@@ -7,7 +11,7 @@ v1 = client.CoreV1Api()
 
 def update_haproxy_config(svc):
     # Логика обновления конфигурации HAProxy
-    print(f"Обновляем конфигурацию HAProxy для сервиса {svc.metadata.name}")
+    print(f"Обновляем конфигурацию HAProxy для сервиса {svc.metadata.name}.{domain}")
     
     # Здесь вы можете добавить код для изменения конфигурации HAProxy,
     # используя, например, библиотеку py-haproxy-config
@@ -19,7 +23,7 @@ def main():
     while True:
         try:
             for event in w.stream(v1.list_service_for_all_namespaces, _request_timeout=60):
-                if event['type'] == 'ADDED' and event['object'].metadata.labels.get('loadbalancer') == 'dynamiclb':
+                if event['type'] == 'ADDED' and event['object'].metadata.labels.get('lbtype') == {lbtype}:
                     service = event['object']
                     if service.spec.type == 'LoadBalancer':
                         update_haproxy_config(svc)
